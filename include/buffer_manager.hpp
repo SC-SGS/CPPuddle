5c9f848426d7ad74af9d67359979483448045d48
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2020-02-14 14:15:35 +0100

    Removed constructors

--- include/buffer_manager.hpp
+++ include/buffer_manager.hpp
@@ -17,8 +17,23 @@ class buffer_recycler {
       if (instance)
         delete instance;
     }
+
+  // Bunch of constructors we don't need
+  public:
+    buffer_recycler(buffer_recycler &other) = delete;
+    buffer_recycler(buffer_recycler const &other) = delete;
+    buffer_recycler operator=(buffer_recycler const &other) = delete;
+    buffer_recycler operator=(buffer_recycler &other) = delete;
+    buffer_recycler(buffer_recycler &&other) = delete;
+    buffer_recycler(buffer_recycler const &&other) = delete;
+    buffer_recycler operator=(buffer_recycler const &&other) = delete;
+    buffer_recycler operator=(buffer_recycler &&other) = delete;
+
   private: 
+    /// Singleton instance pointer
     static buffer_recycler *instance;
+    /// Callbacks for buffer_manager cleanups
+    std::list<std::cuntion
     buffer_recycler(void) {
       std::cout << "Buffer recycler constructor!" << std::endl;
     }
@@ -26,18 +41,40 @@ class buffer_recycler {
       std::cout << "Buffer recycler destructor!" << std::endl;
     }
 
+
+
   private: 
+    /// Memory Manager subclass to handle buffers of specific types and sizes
     template<class T>
     class buffer_manager {
       public:
+        /// Tries to recycle or create a buffer of type T and size number_elements. 
         static void get(size_t number_elements) {
-        if (!instance)
+          if (!instance)
             instance = new buffer_manager();
+          // TODO Check for unused buffers:
+
+          // No unsued buffer found -> Create new one
+          instance->buffer_list.push_back(new T[number_elements]);
+
+          // TODO Return new buffer
         }
 
+      // Bunch of constructors we don't need
+      public:
+        buffer_manager<T>(buffer_manager<T> &other) = delete;
+        buffer_manager<T>(buffer_manager<T> const &other) = delete;
+        buffer_manager<T> operator=(buffer_manager<T> const &other) = delete;
+        buffer_manager<T> operator=(buffer_manager<T> &other) = delete;
+        buffer_manager<T>(buffer_manager<T> &&other) = delete;
+        buffer_manager<T>(buffer_manager<T> const &&other) = delete;
+        buffer_manager<T> operator=(buffer_manager<T> const &&other) = delete;
+        buffer_manager<T> operator=(buffer_manager<T> &&other) = delete;
+
+      // TODO Delete other constructors
       private:
         std::list<T*> buffer_list;
-        static buffer_manager<T> *instance; // requires C++17
+        static buffer_manager<T> *instance; 
 
         buffer_manager(void) {
         std::cout << "Buffer mananger constructor for " << typeid(T).name() << std::endl;
