8815c291eec4b0793350a2c6107cc6f3aa3782f6
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2020-03-05 11:35:01 +0100

    Added view wrapper

--- src/allocator_kokkos_test.cpp
+++ src/allocator_kokkos_test.cpp
@@ -20,6 +20,22 @@ using kokkos_array = Kokkos::View<float[100], Kokkos::HostSpace>;
 using kokkos_pinned_array = Kokkos::View<float[100], Kokkos::CudaHostPinnedSpace>;
 using kokkos_cuda_array = Kokkos::View<float[100], Kokkos::CudaSpace>;
 
+template <class kokkos_type, class alloc_type>
+class recycled_view : public kokkos_type {
+    private:
+        static alloc_type allocator;
+        size_t total_elements;
+    public:
+        template <class... Args>
+        recycled_view(size_t total_elements, Args... args) : kokkos_type(allocator.allocate(total_elements),args...), total_elements(total_elements) {
+        }
+        ~recycled_view(void) {
+            allocator.deallocate(this->data(), total_elements);
+        }
+};
+template <class kokkos_type, class alloc_type>
+alloc_type recycled_view<kokkos_type, alloc_type>::allocator;
+
 // convience function to use the allocators together with Kokkos Views
 template <class T, class... Args>
 std::unique_ptr<T, std::function<void(T *)>> make_or_recycle_unique(Args... args)
@@ -45,12 +61,13 @@ int main(int argc, char *argv[])
 
     std::cout << "Size: "  << sizeof(kokkos_array) << std::endl; // Still a heap allocation!
 
-    // Current Option 1: (the smart (pointer) road)
+    // Bad way: Does not recycle the heap buffer of Kokkos as of yet
     auto input_array = make_or_recycle_unique<kokkos_array>(std::string("my_smart_view"));
     for (size_t i = 0; i < 100; i++) {
         (*input_array)(i) = i * 2.0;
     }
 
+    // Way 1 to recycle heap buffer as well (manually)
     auto my_layout = input_array->layout();
     recycle_std<float> alli;
     float *my_recycled_data_buffer = alli.allocate(100); // allocate memory
@@ -65,43 +82,19 @@ int main(int argc, char *argv[])
     size_t to_alloc = kokkos_um_array::required_allocation_size(100);
     std::cout << "Actual required size: "  << to_alloc << std::endl; // Still a heap allocation!
 
-
-    // Current Option 2: (the manual road)
-    recycle_std<kokkos_array> allocator;
-    kokkos_array *input_array2 = allocator.allocate(1); // allocate memory
-    allocator.construct(input_array2, std::string("my_manual_view")); // initialize kokkos view
+    // Way 2 to recycle 
+    recycled_view<kokkos_um_array, recycle_std<float>> my_wrapper_test0(100);
     for (size_t i = 0; i < 100; i++) {
-        input_array2[0](i) = i * 2.0;
-        (*input_array2)(i) = i *2.0;
-    }
-    allocator.destroy(input_array2);
-    allocator.deallocate(input_array2, 1);
-
-    // Current Option 3: (the vector-save-me road)
-    // Problematic since it wants to initalize with the default view constructor and that one does actually not initialize the kokkos view
-    std::vector<kokkos_array, recycle_std<kokkos_array>> input_array3(1);
-    input_array3[0] = kokkos_array("my_vector_view"); //hence this line...
-    for (size_t i = 0; i < 100; i++) {
-        input_array3[0](i) = i * 2.0;
+        my_wrapper_test0(i) = i * 2.0;
     }
 
-
-    // These should use some of the recycled memory:
-    {
-        auto ptr = make_or_recycle_unique<kokkos_array>();
-        auto ptr1 = make_or_recycle_unique<kokkos_array>();
-        auto ptr2 = make_or_recycle_unique<kokkos_array>();
+    { // Just some views that will be destroyed again to be recylced in the next block
+        recycled_view<kokkos_um_array, recycle_std<float>> my_wrapper_test1(100);
+        recycled_view<kokkos_um_array, recycle_std<float>> my_wrapper_test2(100);
     }
-
-    // These don't work properly because the default constructor of Kokkos::View is useless and does not initialize anything:
-    {
-        std::vector<kokkos_array, recycle_std<kokkos_array>> test1(1);
-        std::vector<kokkos_array, recycle_std<kokkos_array>> test2(1);
+    { // Let the recycling commence
+        recycled_view<kokkos_um_array, recycle_std<float>> my_wrapper_test1(100);
+        recycled_view<kokkos_um_array, recycle_std<float>> my_wrapper_test2(100);
+        recycled_view<kokkos_um_array, recycle_std<float>> my_wrapper_test3(100);
     }
-    {
-        std::vector<kokkos_array, recycle_std<kokkos_array>> test1(1);
-        std::vector<kokkos_array, recycle_std<kokkos_array>> test2(1);
-        std::vector<kokkos_array, recycle_std<kokkos_array>> test3(1);
-    }
-
 }
