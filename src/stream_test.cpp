f0900b6a225aa65ddcd2930a95f08ec4b454e857
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2020-06-24 17:07:14 +0200

    Reduced code duplication in test

--- src/stream_test.cpp
+++ src/stream_test.cpp
@@ -10,315 +10,45 @@
 int main(int argc, char *argv[]) {
 
   std::cout << "Starting priority pool manual test ..." << std::endl;
-  stream_pool::init<cuda_helper, priority_pool<cuda_helper>>(0, 2);
-  {
-    auto test1 =
-        stream_pool::get_interface<cuda_helper, priority_pool<cuda_helper>>();
-    auto load1 = stream_pool::get_current_load<cuda_helper,
-                                               priority_pool<cuda_helper>>();
-    assert(load1 == 0);
-    cuda_helper test1_interface = std::get<0>(test1);
-    size_t test1_index = std::get<1>(test1);
-
-    auto test2 =
-        stream_pool::get_interface<cuda_helper, priority_pool<cuda_helper>>();
-    auto load2 = stream_pool::get_current_load<cuda_helper,
-                                               priority_pool<cuda_helper>>();
-    assert(load2 == 1);
-    cuda_helper test2_interface = std::get<0>(test2);
-    auto fut = test2_interface.get_future();
-    size_t test2_index = std::get<1>(test2);
-
-    auto test3 =
-        stream_pool::get_interface<cuda_helper, priority_pool<cuda_helper>>();
-    auto load3 = stream_pool::get_current_load<cuda_helper,
-                                               priority_pool<cuda_helper>>();
-    assert(load3 == 1);
-    cuda_helper test3_interface = std::get<0>(test3);
-    size_t test3_index = std::get<1>(test3);
-
-    auto test4 =
-        stream_pool::get_interface<cuda_helper, priority_pool<cuda_helper>>();
-    auto load4 = stream_pool::get_current_load<cuda_helper,
-                                               priority_pool<cuda_helper>>();
-    cuda_helper test4_interface = std::get<0>(test4);
-    size_t test4_index = std::get<1>(test4);
-    assert(load4 == 2);
-
-    stream_pool::release_interface<cuda_helper, priority_pool<cuda_helper>>(
-        test4_index);
-    load4 = stream_pool::get_current_load<cuda_helper,
-                                          priority_pool<cuda_helper>>();
-    assert(load4 == 1);
-
-    stream_pool::release_interface<cuda_helper, priority_pool<cuda_helper>>(
-        test3_index);
-    load3 = stream_pool::get_current_load<cuda_helper,
-                                          priority_pool<cuda_helper>>();
-    assert(load3 == 1);
-
-    stream_pool::release_interface<cuda_helper, priority_pool<cuda_helper>>(
-        test2_index);
-    load2 = stream_pool::get_current_load<cuda_helper,
-                                          priority_pool<cuda_helper>>();
-    assert(load2 == 0);
-
-    stream_pool::release_interface<cuda_helper, priority_pool<cuda_helper>>(
-        test1_index);
-    load1 = stream_pool::get_current_load<cuda_helper,
-                                          priority_pool<cuda_helper>>();
-    assert(load1 == 0);
-  }
-  auto load0 =
-      stream_pool::get_current_load<cuda_helper, priority_pool<cuda_helper>>();
-  assert(load0 == 0);
+  test_pool_ref_counting<cuda_helper, priority_pool<cuda_helper>>(0, 2);
   std::cout << "Manual priority pool test successfull!" << std::endl;
   std::cout << std::endl;
 
   std::cout << "Starting priority pool wrapper objects test ..." << std::endl;
-  {
-    hpx_stream_interface_pq test1(0);
-    auto load = stream_pool::get_current_load<cuda_helper,
-                                              priority_pool<cuda_helper>>();
-    assert(load == 0);
-    hpx_stream_interface_pq test2(0);
-    load = stream_pool::get_current_load<cuda_helper,
-                                         priority_pool<cuda_helper>>();
-    auto fut = test2.get_future();
-    assert(load == 1);
-    hpx_stream_interface_pq test3(0);
-    load = stream_pool::get_current_load<cuda_helper,
-                                         priority_pool<cuda_helper>>();
-    assert(load == 1);
-    hpx_stream_interface_pq test4(0);
-    load = stream_pool::get_current_load<cuda_helper,
-                                         priority_pool<cuda_helper>>();
-    assert(load == 2);
-
-    // Check availability method:
-    bool avail =
-        stream_pool::interface_available<cuda_helper,
-                                         priority_pool<cuda_helper>>(1);
-    assert(avail == false); // NOLINT
-    avail = stream_pool::interface_available<cuda_helper,
-                                             priority_pool<cuda_helper>>(2);
-    assert(avail == false); // NOLINT
-    avail = stream_pool::interface_available<cuda_helper,
-                                             priority_pool<cuda_helper>>(3);
-    assert(avail == true); // NOLINT
-  }
-  load0 =
-      stream_pool::get_current_load<cuda_helper, priority_pool<cuda_helper>>();
-  assert(load0 == 0);
+  test_pool_wrappers<cuda_helper, priority_pool<cuda_helper>>(0, 2);
   std::cout << "Wrapper object priority pool test successfull!" << std::endl;
   std::cout << std::endl;
 
   std::cout << "Starting multigpu priority pool wrapper objects test ..."
             << std::endl;
-  stream_pool::init<
+  test_pool_wrappers<
       cuda_helper,
       priority_pool_multi_gpu<cuda_helper, priority_pool<cuda_helper>>>(1, 2);
-  {
-    hpx_stream_interface_mgpq test1(0);
-    auto load = stream_pool::get_current_load<
-        cuda_helper,
-        priority_pool_multi_gpu<cuda_helper, priority_pool<cuda_helper>>>();
-    assert(load == 0);
-    hpx_stream_interface_mgpq test2(0);
-    load = stream_pool::get_current_load<
-        cuda_helper,
-        priority_pool_multi_gpu<cuda_helper, priority_pool<cuda_helper>>>();
-    assert(load == 1);
-    hpx_stream_interface_mgpq test3(0);
-    load = stream_pool::get_current_load<
-        cuda_helper,
-        priority_pool_multi_gpu<cuda_helper, priority_pool<cuda_helper>>>();
-    assert(load == 1);
-    hpx_stream_interface_mgpq test4(0);
-    load = stream_pool::get_current_load<
-        cuda_helper,
-        priority_pool_multi_gpu<cuda_helper, priority_pool<cuda_helper>>>();
-    assert(load == 2);
-
-    // Check availability method:
-    bool avail = stream_pool::interface_available<
-        cuda_helper,
-        priority_pool_multi_gpu<cuda_helper, priority_pool<cuda_helper>>>(1);
-    assert(avail == false); // NOLINT
-    avail = stream_pool::interface_available<
-        cuda_helper,
-        priority_pool_multi_gpu<cuda_helper, priority_pool<cuda_helper>>>(2);
-    assert(avail == false); // NOLINT
-    avail = stream_pool::interface_available<
-        cuda_helper,
-        priority_pool_multi_gpu<cuda_helper, priority_pool<cuda_helper>>>(3);
-    assert(avail == true); // NOLINT
-  }
-  load0 = stream_pool::get_current_load<
-      cuda_helper,
-      priority_pool_multi_gpu<cuda_helper, priority_pool<cuda_helper>>>();
-  assert(load0 == 0);
   std::cout << "Multigpu wrapper object priority pool test successfull!"
             << std::endl;
   std::cout << std::endl;
 
   //   // Round robin pool tests:
   std::cout << "Started manual round-robin pool test ..." << std::endl;
-  stream_pool::init<cuda_helper, round_robin_pool<cuda_helper>>(0, 2);
-  {
-    auto test1 = stream_pool::get_interface<cuda_helper,
-                                            round_robin_pool<cuda_helper>>();
-    auto load1 = stream_pool::get_current_load<cuda_helper,
-                                               round_robin_pool<cuda_helper>>();
-    assert(load1 == 0);
-    cuda_helper test1_interface = std::get<0>(test1);
-    size_t test1_index = std::get<1>(test1);
-
-    auto test2 = stream_pool::get_interface<cuda_helper,
-                                            round_robin_pool<cuda_helper>>();
-    auto load2 = stream_pool::get_current_load<cuda_helper,
-                                               round_robin_pool<cuda_helper>>();
-    assert(load2 == 1);
-    cuda_helper test2_interface = std::get<0>(test1);
-    size_t test2_index = std::get<1>(test2);
-
-    auto test3 = stream_pool::get_interface<cuda_helper,
-                                            round_robin_pool<cuda_helper>>();
-    auto load3 = stream_pool::get_current_load<cuda_helper,
-                                               round_robin_pool<cuda_helper>>();
-    assert(load3 == 1);
-    cuda_helper test3_interface = std::get<0>(test1);
-    size_t test3_index = std::get<1>(test3);
-
-    auto test4 = stream_pool::get_interface<cuda_helper,
-                                            round_robin_pool<cuda_helper>>();
-    auto load4 = stream_pool::get_current_load<cuda_helper,
-                                               round_robin_pool<cuda_helper>>();
-    assert(load4 == 2);
-    cuda_helper test4_interface = std::get<0>(test1);
-    size_t test4_index = std::get<1>(test4);
-
-    stream_pool::release_interface<cuda_helper, round_robin_pool<cuda_helper>>(
-        test4_index);
-    load4 = stream_pool::get_current_load<cuda_helper,
-                                          round_robin_pool<cuda_helper>>();
-    assert(load4 == 1);
-
-    stream_pool::release_interface<cuda_helper, round_robin_pool<cuda_helper>>(
-        test3_index);
-    load3 = stream_pool::get_current_load<cuda_helper,
-                                          round_robin_pool<cuda_helper>>();
-    assert(load3 == 1);
-
-    stream_pool::release_interface<cuda_helper, round_robin_pool<cuda_helper>>(
-        test2_index);
-    load2 = stream_pool::get_current_load<cuda_helper,
-                                          round_robin_pool<cuda_helper>>();
-    assert(load2 == 0);
-
-    stream_pool::release_interface<cuda_helper, round_robin_pool<cuda_helper>>(
-        test1_index);
-    load1 = stream_pool::get_current_load<cuda_helper,
-                                          round_robin_pool<cuda_helper>>();
-    assert(load1 == 0);
-  }
-  load0 = stream_pool::get_current_load<cuda_helper,
-                                        round_robin_pool<cuda_helper>>();
-  assert(load0 == 0);
+  test_pool_ref_counting<cuda_helper, round_robin_pool<cuda_helper>>(0, 2);
   std::cout << "Manual round-robin pool test successfull!" << std::endl;
   std::cout << std::endl;
 
   std::cout << "Starting round-robin pool wrapper objects test ..."
             << std::endl;
-  {
-    hpx_stream_interface_rr test1(0);
-    auto load = stream_pool::get_current_load<cuda_helper,
-                                              round_robin_pool<cuda_helper>>();
-    assert(load == 0);
-    hpx_stream_interface_rr test2(0);
-    load = stream_pool::get_current_load<cuda_helper,
-                                         round_robin_pool<cuda_helper>>();
-    assert(load == 1);
-    hpx_stream_interface_rr test3(0);
-    load = stream_pool::get_current_load<cuda_helper,
-                                         round_robin_pool<cuda_helper>>();
-    assert(load == 1);
-    hpx_stream_interface_rr test4(0);
-    load = stream_pool::get_current_load<cuda_helper,
-                                         round_robin_pool<cuda_helper>>();
-    assert(load == 2);
-
-    // Check availability method:
-    bool avail =
-        stream_pool::interface_available<cuda_helper,
-                                         round_robin_pool<cuda_helper>>(1);
-    assert(avail == false); // NOLINT
-    avail = stream_pool::interface_available<cuda_helper,
-                                             round_robin_pool<cuda_helper>>(2);
-    assert(avail == false); // NOLINT
-    avail = stream_pool::interface_available<cuda_helper,
-                                             round_robin_pool<cuda_helper>>(3);
-    assert(avail == true); // NOLINT
-  }
-  load0 = stream_pool::get_current_load<cuda_helper,
-                                        round_robin_pool<cuda_helper>>();
-  assert(load0 == 0);
+  test_pool_wrappers<cuda_helper, round_robin_pool<cuda_helper>>(0, 2);
   std::cout << "Wrapper object round-robin pool test successfull!" << std::endl;
   std::cout << std::endl;
 
   std::cout << "Starting multigpu round robin pool wrapper objects test... "
             << std::endl;
-  stream_pool::init<
+  test_pool_wrappers<
       cuda_helper,
-      multi_gpu_round_robin_pool<cuda_helper, round_robin_pool<cuda_helper>>>(
-      1, 2);
-  {
-    hpx_stream_interface_mgrr test1(0);
-    auto load = stream_pool::get_current_load<
-        cuda_helper, multi_gpu_round_robin_pool<
-                         cuda_helper, round_robin_pool<cuda_helper>>>();
-    assert(load == 0);
-    hpx_stream_interface_mgrr test2(0);
-    load = stream_pool::get_current_load<
-        cuda_helper, multi_gpu_round_robin_pool<
-                         cuda_helper, round_robin_pool<cuda_helper>>>();
-    assert(load == 1);
-    hpx_stream_interface_mgrr test3(0);
-    load = stream_pool::get_current_load<
-        cuda_helper, multi_gpu_round_robin_pool<
-                         cuda_helper, round_robin_pool<cuda_helper>>>();
-    assert(load == 1);
-    hpx_stream_interface_mgrr test4(0);
-    load = stream_pool::get_current_load<
-        cuda_helper, multi_gpu_round_robin_pool<
-                         cuda_helper, round_robin_pool<cuda_helper>>>();
-    assert(load == 2);
-
-    // Check availability method:
-    bool avail = stream_pool::interface_available<
-        cuda_helper,
-        multi_gpu_round_robin_pool<cuda_helper, round_robin_pool<cuda_helper>>>(
-        1);
-    assert(avail == false); // NOLINT
-    avail = stream_pool::interface_available<
-        cuda_helper,
-        multi_gpu_round_robin_pool<cuda_helper, round_robin_pool<cuda_helper>>>(
-        2);
-    assert(avail == false); // NOLINT
-    avail = stream_pool::interface_available<
-        cuda_helper,
-        multi_gpu_round_robin_pool<cuda_helper, round_robin_pool<cuda_helper>>>(
-        3);
-    assert(avail == true); // NOLINT
-  }
-  load0 = stream_pool::get_current_load<
-      cuda_helper,
-      multi_gpu_round_robin_pool<cuda_helper, round_robin_pool<cuda_helper>>>();
-  assert(load0 == 0);
+      priority_pool_multi_gpu<cuda_helper, priority_pool<cuda_helper>>>(1, 2);
   std::cout << "Multigpu wrapper object round robin pool test successfull!"
             << std::endl;
-  std::cout << std::endl;
 
+  std::cout << "Starting memcpy tests... " << std::endl;
   test_pool_memcpy<cuda_helper, round_robin_pool<cuda_helper>>(0, 2);
   test_pool_memcpy<
       cuda_helper,
@@ -328,4 +58,14 @@ int main(int argc, char *argv[]) {
   test_pool_memcpy<
       cuda_helper,
       priority_pool_multi_gpu<cuda_helper, priority_pool<cuda_helper>>>(1, 2);
+
+  // combo pool
+  test_pool_memcpy<cuda_helper, multi_gpu_round_robin_pool<
+                                    cuda_helper, priority_pool<cuda_helper>>>(
+      1, 2);
+  test_pool_memcpy<
+      cuda_helper,
+      priority_pool_multi_gpu<cuda_helper, round_robin_pool<cuda_helper>>>(1,
+                                                                           2);
+  std::cout << "Finished memcpy tests! " << std::endl;
 }
--- src/stream_test.hpp
+++ src/stream_test.hpp
@@ -1,5 +1,5 @@
-#ifndef STREAM_TEST_HPP
-#define STREAM_TEST_HPP
+#ifndef STREAM_TEST_HPP // NOLINT
+#define STREAM_TEST_HPP // NOLINT
 #include "../include/buffer_manager.hpp"
 #include "../include/cuda_buffer_util.hpp"
 
@@ -34,6 +34,85 @@ void test_pool_memcpy(const size_t gpu_parameter,
     auto fut1 = test1_interface.get_future();
     fut1.get();
   }
+}
+
+template <typename Interface, typename Pool>
+void test_pool_ref_counting(const size_t gpu_parameter,
+                            size_t stream_parameter) {
+
+  // init ppol
+  stream_pool::init<Interface, Pool>(gpu_parameter, stream_parameter);
+  {
+    // Allocating
+    auto test1 = stream_pool::get_interface<Interface, Pool>();
+    auto load1 = stream_pool::get_current_load<Interface, Pool>();
+    assert(load1 == 0);
+    Interface test1_interface = std::get<0>(test1);
+    size_t test1_index = std::get<1>(test1);
+    auto test2 = stream_pool::get_interface<Interface, Pool>();
+    auto load2 = stream_pool::get_current_load<Interface, Pool>();
+    assert(load2 == 1);
+    Interface test2_interface = std::get<0>(test2);
+    auto fut = test2_interface.get_future();
+    size_t test2_index = std::get<1>(test2);
+    auto test3 = stream_pool::get_interface<Interface, Pool>();
+    auto load3 = stream_pool::get_current_load<Interface, Pool>();
+    assert(load3 == 1);
+    Interface test3_interface = std::get<0>(test3);
+    size_t test3_index = std::get<1>(test3);
+    auto test4 = stream_pool::get_interface<Interface, Pool>();
+    auto load4 = stream_pool::get_current_load<Interface, Pool>();
+    Interface test4_interface = std::get<0>(test4);
+    size_t test4_index = std::get<1>(test4);
+    assert(load4 == 2);
+    // Releasing
+    stream_pool::release_interface<Interface, Pool>(test4_index);
+    load4 = stream_pool::get_current_load<Interface, Pool>();
+    assert(load4 == 1);
+    stream_pool::release_interface<Interface, Pool>(test3_index);
+    load3 = stream_pool::get_current_load<Interface, Pool>();
+    assert(load3 == 1);
+    stream_pool::release_interface<Interface, Pool>(test2_index);
+    load2 = stream_pool::get_current_load<Interface, Pool>();
+    assert(load2 == 0);
+    stream_pool::release_interface<Interface, Pool>(test1_index);
+    load1 = stream_pool::get_current_load<Interface, Pool>();
+    assert(load1 == 0);
+  }
+  // Clear
+  auto load0 = stream_pool::get_current_load<Interface, Pool>();
+  assert(load0 == 0);
+}
+
+template <typename Interface, typename Pool>
+void test_pool_wrappers(const size_t gpu_parameter, size_t stream_parameter) {
+  using wrapper_type = stream_interface<Interface, Pool>;
+  // init ppol
+  stream_pool::init<Interface, Pool>(gpu_parameter, stream_parameter);
+  {
+    wrapper_type test1(0);
+    auto load = stream_pool::get_current_load<Interface, Pool>();
+    assert(load == 0);
+    wrapper_type test2(0);
+    load = stream_pool::get_current_load<Interface, Pool>();
+    auto fut = test2.get_future();
+    assert(load == 1);
+    wrapper_type test3(0);
+    load = stream_pool::get_current_load<Interface, Pool>();
+    assert(load == 1);
+    wrapper_type test4(0);
+    load = stream_pool::get_current_load<Interface, Pool>();
+    assert(load == 2);
+    // Check availability method:
+    bool avail = stream_pool::interface_available<Interface, Pool>(1);
+    assert(avail == false); // NOLINT
+    avail = stream_pool::interface_available<Interface, Pool>(2);
+    assert(avail == false); // NOLINT
+    avail = stream_pool::interface_available<Interface, Pool>(3);
+    assert(avail == true); // NOLINT
+  }
+  auto load0 = stream_pool::get_current_load<Interface, Pool>();
+  assert(load0 == 0);
 }
 
 #endif
