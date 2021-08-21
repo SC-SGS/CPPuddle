cfe9e130adc44f485ce6f00b261e2966bb439cd7
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2021-08-21 23:31:48 +0200

    Fix build

--- scripts/machine_configs.sh
+++ scripts/machine_configs.sh
@@ -63,6 +63,32 @@ function source_config_daint() {
   module load cray-jemalloc
 }
 
+function source_config_xp14() {
+  SCRIPTS_DIR="$1"
+  export CMAKE_BUILD_TYPE="$2"
+  if [[ "${3}" == "gcc" ]]; then
+    export CXX=${SCRIPTS_DIR}/../external_dependencies/kokkos/bin/nvcc_wrapper
+    export HPX_COMPILER="g++"
+  elif [[ "${3}" == "clang" ]]; then
+    export CXX=clang++
+    export HPX_COMPILER=clang++
+  else
+    echo "Invalid compiler!"
+    exit 1
+  fi
+  export APPEND_DIRNAME="-$3-$2"
+
+
+  #export CXX=${SCRIPTS_DIR}/../external_dependencies/kokkos/bin/nvcc_wrapper
+  export CXXFLAGS="-Wno-cpp" # Silence deprecated header warnings in HPX
+  #export CMAKE_BUILD_TYPE=Release
+  export HPX_ROOT=${SCRIPTS_DIR}/../external_dependencies/install/hpx-${APPEND_DIRNAME}/lib/cmake/HPX
+  export Kokkos_ROOT=${SCRIPTS_DIR}/../external_dependencies/install/kokkos-${APPEND_DIRNAME}/lib/cmake/Kokkos
+  export HPXKokkos_ROOT=${SCRIPTS_DIR}/../external_dependencies/install/kokkos-hpx-interop-${APPEND_DIRNAME}/lib/cmake/HPXKokkos
+
+  export CURRENT_CUDA_ARCH_FLAG="-DKokkos_ARCH_SKX=ON -DKokkos_ARCH_TURING75=ON"
+}
+
 function source_config_default() {
   echo -e "\033[33mWARNING: Default configuration... You likely need to modify this\033[0m"
   sleep 8 
