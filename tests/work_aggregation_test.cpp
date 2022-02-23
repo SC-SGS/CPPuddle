9a90cb007f08662b0c1c7408512cb7ed858a7a21
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2022-02-22 18:05:02 -0600

    Added basic work aggregation allocator

--- tests/work_aggregation_test.cpp
+++ tests/work_aggregation_test.cpp
@@ -81,6 +81,13 @@ void print_stuff3(const std::atomic<int> &used_slices, int i) {
   const int bla = used_slices;
   hpx::cout << "i is " << i << "(Slice " << bla << ")" << std::endl;
 }
+
+void add(const float *const A, const float *const B, float *const C,
+         size_t size) {
+  for (size_t i = 0; i < size; i++) {
+    C[i] = B[i] + A[i];
+  }
+}
 /*void print_stuff1(int *used_slices, int i) {
   hpx::cout << "i is " << i << "(Slice " << *used_slices << ")" << std::endl;
 }
@@ -233,27 +240,24 @@ public:
             << "\nMismatched types error in aggregated post call of executor "
             << kernelname << ": " << e.what() << "\n";
         hpx::cout << "Expected types:\t\t "
-                  << boost::core::demangle(debug_type_information.c_str())
-                  << "\n";
-        hpx::cout << "Got types:\t\t "
+                  << boost::core::demangle(debug_type_information.c_str());
+        hpx::cout << "\nGot types:\t\t "
                   << boost::core::demangle(
                          typeid(decltype(comparison_tuple)).name())
-                  << "\n";
-        hpx::cout << std::endl;
+                  << "\n"
+                  << std::endl;
         // throw;
       } catch (const std::runtime_error &e) {
         hpx::cout
             << "\nMismatched values error in aggregated post call of executor "
             << kernelname << ": " << e.what() << std::endl;
         hpx::cout << "Types (matched):\t "
-                  << boost::core::demangle(debug_type_information.c_str())
-                  << "\n";
+                  << boost::core::demangle(debug_type_information.c_str());
         auto orig_call_tuple =
             std::any_cast<decltype(comparison_tuple)>(function_tuple);
-        hpx::cout << "Expected values:\t ";
+        hpx::cout << "\nExpected values:\t ";
         print_tuple(orig_call_tuple);
-        hpx::cout << "\n";
-        hpx::cout << "Got values:\t\t ";
+        hpx::cout << "\nGot values:\t\t ";
         print_tuple(comparison_tuple);
         hpx::cout << std::endl << std::endl;
         // throw;
@@ -324,27 +328,24 @@ public:
             << "\nMismatched types error in aggregated async call of executor "
             << kernelname << ": " << e.what() << "\n";
         hpx::cout << "Expected types:\t\t "
-                  << boost::core::demangle(debug_type_information.c_str())
-                  << "\n";
-        hpx::cout << "Got types:\t\t "
+                  << boost::core::demangle(debug_type_information.c_str());
+        hpx::cout << "\nGot types:\t\t "
                   << boost::core::demangle(
                          typeid(decltype(comparison_tuple)).name())
-                  << "\n";
-        hpx::cout << std::endl;
+                  << "\n"
+                  << std::endl;
         // throw;
       } catch (const std::runtime_error &e) {
         hpx::cout
             << "\nMismatched values error in aggregated async call of executor "
             << kernelname << ": " << e.what() << std::endl;
         hpx::cout << "Types (matched):\t "
-                  << boost::core::demangle(debug_type_information.c_str())
-                  << "\n";
+                  << boost::core::demangle(debug_type_information.c_str());
         auto orig_call_tuple =
             std::any_cast<decltype(comparison_tuple)>(function_tuple);
-        hpx::cout << "Expected values:\t ";
+        hpx::cout << "\nExpected values:\t ";
         print_tuple(orig_call_tuple);
-        hpx::cout << "\n";
-        hpx::cout << "Got values:\t\t ";
+        hpx::cout << "\nGot values:\t\t ";
         print_tuple(comparison_tuple);
         hpx::cout << std::endl << std::endl;
         // throw;
@@ -389,9 +390,8 @@ private:
   bool slices_exhausted;
   const size_t max_slices;
   size_t current_slices;
-  std::mutex mut;
 
-  //===============================================================================
+public:
   // Subclasses
 
   /// Slice class - meant as a scope interface to the aggregated executor
@@ -405,10 +405,13 @@ private:
     /// How many functions have been called - required to enforce sequential
     /// behaviour of kernel launches
     size_t launch_counter{0};
+    size_t buffer_counter{0};
 
   public:
-    Executor_Slice(Aggregated_Executor &parent, const size_t number_slices)
-        : parent(parent), number_slices(number_slices) {}
+    const size_t id;
+    Executor_Slice(Aggregated_Executor &parent, const size_t slice_id,
+                   const size_t number_slices)
+        : parent(parent), number_slices(number_slices), id(slice_id) {}
     template <typename F, typename... Ts> void post(F &&f, Ts &&...ts) {
       // we should only execute function calls once all slices
       // have been given away (-> Executor Slices start)
@@ -426,7 +429,29 @@ private:
       launch_counter++;
       return ret_fut;
     }
+
+    /// Get new aggregated buffer (might have already been allocated been
+    /// allocated by different slice)
+    template <typename T, typename Host_Allocator> T *get(const size_t size) {
+      T *aggregated_buffer =
+          parent.get<T, Host_Allocator>(size, buffer_counter);
+      buffer_counter++;
+      return aggregated_buffer;
+    }
+    /// Mark aggregated buffer as unused (should only happen due to stack unwinding)
+    /// Do not call manually
+    template <typename T, typename Host_Allocator>
+    void mark_unused(T *p, const size_t size,
+                     const size_t slice_alloc_counter) {
+      buffer_counter--;
+      parent.mark_unused<T, Host_Allocator>(p, size, buffer_counter);
+    }
+    // TODO Support the reference counting used previousely in CPPuddle?
+    // might be required for Kokkos
+
+    
   };
+
   //===============================================================================
 
   /// Promises with the slice executors -- to be set when the starting criteria
@@ -435,6 +460,84 @@ private:
   /// List of aggregated function calls - function will be launched when all
   /// slices have called it
   std::list<aggregated_function_call<kernelname, Executor>> function_calls;
+  /// For synchronizing the access to the function calls list
+  std::mutex mut;
+
+  // TODO Evaluate if we should switch to boost::any and unsafe casts (=> should
+  // be faster)
+  /// Data entry for a buffer allocation: any for the point, size_t for
+  /// buffer-size, atomic for the slice counter
+  using buffer_entry_t =
+      std::tuple<std::any, const size_t, std::atomic<size_t>>;
+  /// Keeps track of the aggregated buffer allocations done in all the slices
+  std::deque<buffer_entry_t> buffer_allocations;
+  /// For synchronizing the access to the buffer_allocations
+  std::mutex buffer_mut;
+
+  /// Get new buffer OR get buffer already allocated by different slice
+  template <typename T, typename Host_Allocator>
+  T *get(const size_t size, const size_t slice_alloc_counter) {
+    // Add aggreated buffer entry in case it hasn't happened yet for this call
+    // First: Check if it already has happened
+    if (buffer_allocations.size() <= slice_alloc_counter) {
+      // we might be the first! Lock...
+      std::lock_guard<std::mutex> guard(buffer_mut);
+      // ... and recheck
+      if (function_calls.size() <= slice_alloc_counter) {
+        // Get shiny and new buffer that will be shared between all slices
+        // Buffer might be recycled from previous allocations by the
+        // buffer_recycler...
+        T *aggregated_buffer =
+            recycler::detail::buffer_recycler::get<T, Host_Allocator>(size,
+                                                                      true);
+        // Create buffer entry for this buffer
+        buffer_allocations.emplace_back(aggregated_buffer, size, 1);
+        // Return buffer
+        return aggregated_buffer;
+      }
+    }
+
+    // Buffer entry should already exist:
+    T *aggregated_buffer = std::any_cast<T *>(
+        std::get<0>(buffer_allocations[slice_alloc_counter]));
+    // Error handling: Size is wrong
+    if (size != std::get<1>(buffer_allocations[slice_alloc_counter])) {
+      throw std::runtime_error("Requested buffer size does not match the size "
+                               "in the aggregated buffer record!");
+    }
+    // Notify that one more slice has visited this buffer allocation
+    std::get<2>(buffer_allocations[slice_alloc_counter])++;
+    return aggregated_buffer;
+  }
+
+  /// Notify buffer list that one slice is done with the buffer
+  template <typename T, typename Host_Allocator>
+  void mark_unused(T *p, const size_t size, const size_t slice_alloc_counter) {
+    assert(slice_alloc_counter < buffer_allocations.size());
+    assert(std::get<1>(buffer_allocations[slice_alloc_counter]) == size);
+    // Slice is done with this buffer
+    std::get<2>(buffer_allocations[slice_alloc_counter])--;
+    // Check if all buffers are done?
+    if (std::get<2>(buffer_allocations[slice_alloc_counter]) == 0) {
+      // Yes! "Deallocate" by telling the recylcer the buffer is fit for reusage
+      std::lock_guard<std::mutex> guard(buffer_mut);
+      T *aggregated_buffer = std::any_cast<T *>(
+          std::get<0>(buffer_allocations[slice_alloc_counter]));
+      // Only mark unused if another buffer has not done so already (and marked
+      // it as nullptr)
+      if (aggregated_buffer != nullptr) {
+        assert(p == aggregated_buffer);
+        const size_t buffer_size =
+            std::get<1>(buffer_allocations[slice_alloc_counter]);
+        recycler::detail::buffer_recycler::mark_unused<T, Host_Allocator>(
+            aggregated_buffer, buffer_size);
+        // mark as nullptr
+        std::get<0>(buffer_allocations[slice_alloc_counter]) = nullptr;
+      }
+    }
+  }
+  // TODO Support the reference counting used previousely in CPPuddle?
+  // might be required for Kokkos
 
   //===============================================================================
   // Public Interface
@@ -494,22 +597,27 @@ public:
         // TODO get future and add continuation for when the stream does its
         // thing
         auto fut = dummy_stream_promise.get_future();
-        current_continuation = fut.then([this,
-                                         kernelname = kernelname](auto &&fut) {
-          std::lock_guard<std::mutex> guard(mut);
-          if (!slices_exhausted) {
-            slices_exhausted = true;
-            for (auto &slice_promise : executor_slices) {
-              slice_promise.set_value(Executor_Slice{*this, current_slices});
-            }
-            executor_slices.clear();
-          }
-        });
+        current_continuation =
+            fut.then([this, kernelname = kernelname](auto &&fut) {
+              std::lock_guard<std::mutex> guard(mut);
+              if (!slices_exhausted) {
+                slices_exhausted = true;
+                size_t id = 0;
+                for (auto &slice_promise : executor_slices) {
+                  slice_promise.set_value(
+                      Executor_Slice{*this, id, current_slices});
+                  id++;
+                }
+                executor_slices.clear();
+              }
+            });
       }
       if (current_slices == max_slices) {
         slices_exhausted = true;
+        size_t id = 0;
         for (auto &slice_promise : executor_slices) {
-          slice_promise.set_value(Executor_Slice{*this, current_slices});
+          slice_promise.set_value(Executor_Slice{*this, id, current_slices});
+          id++;
         }
         executor_slices.clear();
       }
@@ -536,6 +644,50 @@ public:
   Aggregated_Executor(Aggregated_Executor &&other) = delete;
   Aggregated_Executor &operator=(Aggregated_Executor &&other) = delete;
 };
+
+template <typename T, typename Host_Allocator, const char *kernelname, typename Executor>
+class Allocator_Slice {
+  private:
+  typename Aggregated_Executor<kernelname,Executor>::Executor_Slice &executor_reference;
+  public:
+  using value_type = T;
+  Allocator_Slice(typename Aggregated_Executor<kernelname, Executor>::Executor_Slice &executor) : executor_reference(executor) {
+  }
+  template <typename U>
+  explicit Allocator_Slice(
+      Allocator_Slice<U, Host_Allocator, kernelname, Executor> const &) noexcept {}
+  T *allocate(std::size_t n) {
+    T *data = executor_reference.template get<T, Host_Allocator>(n); 
+    return data;
+  }
+  void deallocate(T *p, std::size_t n) {
+    executor_reference.template mark_unused<T, Host_Allocator>(p, n);
+  }
+  template <typename... Args>
+  inline void construct(T *p, Args... args) noexcept {
+    // Do nothing here - we reuse the content of the last owner
+  }
+  void destroy(T *p) {
+    // Do nothing here - Contents will be destroyed when the buffer manager is
+    // destroyed, not before
+  }
+  // TODO Reference counting not supported yet
+  /*void increase_usage_counter(T *p, size_t n) {
+    buffer_recycler::increase_usage_counter<T, Host_Allocator>(p, n);
+  }*/
+};
+template <typename T, typename U, typename Host_Allocator, const char *kernelname, typename Executor>
+constexpr bool
+operator==(Allocator_Slice<T, Host_Allocator, kernelname, Executor> const &,
+           Allocator_Slice<U, Host_Allocator, kernelname, Executor> const &) noexcept {
+  return true;
+}
+template <typename T, typename U, typename Host_Allocator, const char *kernelname, typename Executor>
+constexpr bool
+operator!=(Allocator_Slice<T, Host_Allocator, kernelname, Executor> const &,
+           Allocator_Slice<U, Host_Allocator, kernelname, Executor> const &) noexcept {
+  return false;
+}
 //===============================================================================
 //===============================================================================
 
@@ -597,6 +749,11 @@ int hpx_main(int argc, char *argv[]) {
     std::vector<hpx::lcos::future<void>> slices_done_futs;
     slices_done_futs.emplace_back(slice_fut1.then([](auto &&fut) {
       auto slice_exec = fut.get();
+      Allocator_Slice<float, std::allocator<float>, kernelname1, decltype(executor1)> alloc(slice_exec);
+      hpx::cout << "Executor 1 ID is " << slice_exec.id << std::endl;
+      float *some_data = alloc.allocate(4 * 10);
+      hpx::cout << "Executor 1 Data address is " << some_data << std::endl;
+
       slice_exec.post(print_stuff1, 1);
       slice_exec.post(print_stuff2, 1, 1.0);
       auto kernel_fut = slice_exec.async(print_stuff1, 1);
@@ -606,6 +763,10 @@ int hpx_main(int argc, char *argv[]) {
     auto slice_fut2 = agg_exec.request_executor_slice();
     slices_done_futs.emplace_back(slice_fut2.then([](auto &&fut) {
       auto slice_exec = fut.get();
+      Allocator_Slice<float, std::allocator<float>, kernelname1, decltype(executor1)> alloc(slice_exec);
+      hpx::cout << "Executor 2 ID is " << slice_exec.id << std::endl;
+      float *some_data = alloc.allocate(4 * 10);
+      hpx::cout << "Executor 2 Data address is " << some_data << std::endl;
       slice_exec.post(print_stuff1, 1);
       slice_exec.post(print_stuff2, 1, 1.0);
       auto kernel_fut = slice_exec.async(print_stuff1, 1);
@@ -615,6 +776,10 @@ int hpx_main(int argc, char *argv[]) {
     auto slice_fut3 = agg_exec.request_executor_slice();
     slices_done_futs.emplace_back(slice_fut3.then([](auto &&fut) {
       auto slice_exec = fut.get();
+      Allocator_Slice<float, std::allocator<float>, kernelname1, decltype(executor1)> alloc(slice_exec);
+      hpx::cout << "Executor 3 ID is " << slice_exec.id << std::endl;
+      float *some_data = alloc.allocate(4 * 10);
+      hpx::cout << "Executor 3 Data address is " << some_data << std::endl;
       slice_exec.post(print_stuff1, 1);
       slice_exec.post(print_stuff2, 1, 1.0);
       auto kernel_fut = slice_exec.async(print_stuff1, 1);
@@ -624,6 +789,10 @@ int hpx_main(int argc, char *argv[]) {
     auto slice_fut4 = agg_exec.request_executor_slice();
     slices_done_futs.emplace_back(slice_fut4.then([](auto &&fut) {
       auto slice_exec = fut.get();
+      Allocator_Slice<float, std::allocator<float>, kernelname1, decltype(executor1)> alloc(slice_exec);
+      hpx::cout << "Executor 4 ID is " << slice_exec.id << std::endl;
+      float *some_data = alloc.allocate(4 * 10);
+      hpx::cout << "Executor 4 Data address is " << some_data << std::endl;
       slice_exec.post(print_stuff1, 1);
       slice_exec.post(print_stuff2, 1, 1.0);
       auto kernel_fut = slice_exec.async(print_stuff1, 1);
@@ -653,6 +822,8 @@ int hpx_main(int argc, char *argv[]) {
       hpx::cout << "Got executor 1" << std::endl;
       slice_exec.post(print_stuff1, 1);
       slice_exec.post(print_stuff2, 1, 1.0);
+      auto kernel_fut = slice_exec.async(print_stuff1, 1);
+      kernel_fut.get();
     }));
 
     auto slice_fut2 = agg_exec.request_executor_slice();
@@ -661,6 +832,8 @@ int hpx_main(int argc, char *argv[]) {
       hpx::cout << "Got executor 2" << std::endl;
       slice_exec.post(print_stuff1, 1);
       slice_exec.post(print_stuff2, 1, 1.0);
+      auto kernel_fut = slice_exec.async(print_stuff1, 1);
+      kernel_fut.get();
     }));
 
     auto slice_fut3 = agg_exec.request_executor_slice();
@@ -669,6 +842,8 @@ int hpx_main(int argc, char *argv[]) {
       hpx::cout << "Got executor 3" << std::endl;
       slice_exec.post(print_stuff1, 1);
       slice_exec.post(print_stuff2, 1, 1.0);
+      auto kernel_fut = slice_exec.async(print_stuff1, 1);
+      kernel_fut.get();
     }));
 
     hpx::cout << "Requested 3 executors!" << std::endl;
@@ -681,7 +856,7 @@ int hpx_main(int argc, char *argv[]) {
   hpx::cout << std::endl;
 
   // Error test
-  /* hpx::cout << "Error test with all wrong types and values in 2 slices"
+  /*hpx::cout << "Error test with all wrong types and values in 2 slices"
             << std::endl;
   hpx::cout << "------------------------------------------------------"
             << std::endl;
@@ -738,6 +913,8 @@ int hpx_main(int argc, char *argv[]) {
   }
   hpx::cout << std::endl;*/
   hpx::cout << "Done!" << std::endl;
+  hpx::cout << std::endl;
+  std::cin.get();
 
   recycler::force_cleanup(); // Cleanup all buffers and the managers for better
   return hpx::finalize();
