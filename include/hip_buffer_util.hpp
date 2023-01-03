266f456c2966526f57bb41379f2c64344c11e612
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2023-01-03 14:12:22 +0100

    Add experimental sycl default allocator

--- include/hip_buffer_util.hpp
+++ include/hip_buffer_util.hpp
@@ -3,8 +3,8 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#ifndef CUDA_BUFFER_UTIL_HPP
-#define CUDA_BUFFER_UTIL_HPP
+#ifndef HIP_BUFFER_UTIL_HPP
+#define HIP_BUFFER_UTIL_HPP
 
 #include "buffer_manager.hpp"
 
