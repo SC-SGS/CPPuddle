104c84177669ca56de631991b8eac02c174641eb
G-071 <G-071@users.noreply.github.com>
2024-03-11 12:53:57 +0000

    deploy: 3789044922a8e3d6ffcd21294fb547f3f2174f9c

--- search/typedefs_9.js
+++ search/typedefs_9.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['stream_5finterface_528',['stream_interface',['../stream__manager_8hpp.html#a942e2f724931eb71d0ed6459027c95fb',1,'stream_manager.hpp']]],
   ['stream_5fpool_529',['stream_pool',['../stream__manager_8hpp.html#aaae2624dc57ec7eea387f73d919617be',1,'stream_manager.hpp']]],
-  ['sycl_5fdevice_5fdefault_5fallocator_530',['sycl_device_default_allocator',['../namespacerecycler_1_1detail.html#a79ecd1b1de4bdcab51b373d6e5e16355',1,'recycler::detail']]],
-  ['sycl_5fhost_5fdefault_5fallocator_531',['sycl_host_default_allocator',['../namespacerecycler_1_1detail.html#a1dab9c7c4588b33f933337009a61658d',1,'recycler::detail']]]
+  ['sycl_5fdevice_5fdefault_5fallocator_530',['sycl_device_default_allocator',['../namespacerecycler_1_1detail.html#a9aaf001c44760b2c9a599ed73b8a18b7',1,'recycler::detail']]],
+  ['sycl_5fhost_5fdefault_5fallocator_531',['sycl_host_default_allocator',['../namespacerecycler_1_1detail.html#aece53e70ac832ebb5cf8644d713b9a12',1,'recycler::detail']]]
 ];
